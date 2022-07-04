d07a4af4154bebd1ba20c1e7eca3761ccc8772f6
Arjun Taneja <arjun.taneja02@gmail.com>
2022-07-04 16:56:57 +0800

    Publish to server

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use wasm_bindgen::prelude::*;
 use eng::euler::{Vec3, Vec4, Mat3, Mat4};
 
 
+
 extern crate web_sys;
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
@@ -24,18 +25,6 @@ macro_rules! log {
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("{}!", name));
-}
-
-#[wasm_bindgen]
 pub struct GlobalWrapper {
     lines: Vec<f64>,
     renderer: eng::renderer::Renderer,
@@ -46,12 +35,27 @@ pub struct GlobalWrapper {
     angular_velocity: eng::euler::Mat4
 }
 
+#[wasm_bindgen] 
+pub fn generate_structures() { 
+    // Hardcoded cube
+    let vec0 = Vec3::new(-25.0, -25.0, -25.0);
+    let vec1 = Vec3::new(25.0, -25.0, -25.0);
+    let vec2 = Vec3::new(-25.0, 25.0, -25.0);
+    let vec3 = Vec3::new(25.0, 25.0, -25.0);
+    let vec4 = Vec3::new(-25.0, -25.0, 25.0);
+    let vec5 = Vec3::new(25.0, -25.0, 25.0);
+    let vec6 = Vec3::new(-25.0, 25.0, 25.0);
+    let vec7 = Vec3::new(25.0, 25.0, 25.0);
+
+    let vec_points: Vec<Vec3> = vec![vec0, vec1, vec2, vec3, vec4, vec5, vec6, vec7];
+    let vec_connections: Vec<usize> = vec![0, 1, 1, 3, 3, 2, 2, 0, 4, 5, 5, 7, 7, 6, 6, 4, 0, 4, 1, 5, 2, 6, 3, 7];
+
+}
+
 #[wasm_bindgen]
 impl GlobalWrapper {
     pub fn new() -> GlobalWrapper { 
         let mut renderer = eng::renderer::Renderer::default();
-
-        let camera  = eng::renderer::Camera::default();let mut renderer = eng::renderer::Renderer::default();
         renderer.update_aspect_ratio(1.158);
 
         let camera  = eng::renderer::Camera::default();
